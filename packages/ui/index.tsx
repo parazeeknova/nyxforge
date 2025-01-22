8db8333c3ade43be3ec5ceebf4a1e1c6b737edf2
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-01-22 20:06:58 +0530

    feat[obs&error]: Added sentry, posthog & other analytics packages
    Need more package configs soon

---
+++ packages/ui/index.tsx
@@ -0,0 +1,19 @@
+import { AnalyticsProvider } from '@forge/analytics';
+import type { ThemeProviderProps } from 'next-themes';
+import { ThemeProvider } from './providers/theme';
+import { Toaster } from './shad-components/ui/sonner';
+import { TooltipProvider } from './shad-components/ui/tooltip';
+
+type DesignSystemProviderProperties = ThemeProviderProps;
+
+export const DesignSystemProvider = ({
+  children,
+  ...properties
+}: DesignSystemProviderProperties) => (
+  <ThemeProvider {...properties}>
+    <AnalyticsProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+      <Toaster />
+    </AnalyticsProvider>
+  </ThemeProvider>
+);
---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export { config as default } from '@forge/tailwind';
