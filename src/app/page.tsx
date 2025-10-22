1338b90a5741f6ead12b47fb1de41720cad7e5e6
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-10-23 00:37:46 +0530

    feat[LINT]: add ultracite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black">
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={200}
-        height={200}
-      />
+      <Image alt="Vercel Logo" height={200} src="/vercel.svg" width={200} />
     </main>
   );
 }
